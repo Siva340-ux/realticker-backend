355a524729ab5dd54a952958af9d1603d9a61a9e
Siva <sivaofficial370@gmail.com>
2026-04-10 00:07:21 +0530

    Fix component scanning

--- src/main/java/com/siva/realticker/RealtickerBackendApplication.java
+++ src/main/java/com/siva/realticker/RealtickerBackendApplication.java
@@ -2,8 +2,10 @@ package com.siva.realticker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.siva.realticker")
 public class RealtickerBackendApplication {
 
 	public static void main(String[] args) {
